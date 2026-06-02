d34bbac101b49a176338c14b9f66afb960b66006
Aditya Tyagi <saumaytyagi2003@gmail.com>
2026-06-02 13:46:58 +0530

    Fix CORS for Vercel Frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,12 +18,14 @@ app = FastAPI(
     title="Inventory Management API"
 )
 
+origins = [
+    "http://localhost:5173",
+    "https://inventory-management-plum-five.vercel.app"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-    "http://localhost:5173",
-    "https://inventory-management-plum-five.vercel.app/"
-],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
